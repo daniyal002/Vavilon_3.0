d075f3424c52cbc99d7d7751e0f363e8e4960a68
daniyal002 <daniyalou@gmail.com>
2025-01-21 14:41:11 +0300

    capacitor init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ru.kinovavilon.app',
+  appName: 'vavilon-mobile',
+  webDir: 'dist'
+};
+
+export default config;
