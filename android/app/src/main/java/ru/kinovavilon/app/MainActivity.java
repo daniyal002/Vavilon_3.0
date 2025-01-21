3e5ccd50b592d09332a7763ab04fedd20d4b9343
daniyal002 <daniyalou@gmail.com>
2025-01-21 15:51:49 +0300

    delete ios and android

--- android/app/src/main/java/ru/kinovavilon/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package ru.kinovavilon.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'ru.kinovavilon.app',
-  appName: 'vavilon-mobile',
-  webDir: 'dist'
-};
-
-export default config;
